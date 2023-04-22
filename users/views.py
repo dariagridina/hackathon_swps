ce38dc1cb6dc91aac2b40afd6306ab06ebd6feb9
Igor Zmitrovich <zmitrovich@smartpatient.eu>
2023-04-22 18:32:36 +0200

    Add filtering of people

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.db import models
+from django.db.models import Q
 from django.views.generic import DetailView, ListView
 
-from users.models import User
+from users.models import User, UserSkill
 from users.forms import LoginForm
 
 
@@ -26,4 +27,25 @@ class UserProfileListView(ListView):
     model = User
 
     def get_queryset(self):
-        return User.objects.filter(profile__isnull=False)
+        qs = User.objects.filter(profile__isnull=False)
+        skill = self.request.GET.get("skill")
+        if skill:
+            print("skill: ", skill)
+            qs = qs.filter(skill__name=skill)
+        city = self.request.GET.get("city")
+        if city:
+            print("city: ", city)
+            qs = qs.filter(profile__location=city)
+        name = self.request.GET.get("name")
+        if name:
+            print("name: ", name)
+            qs = qs.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
+        return qs.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        qs = self.get_queryset()
+        if qs.exists():
+            context['skills'] = list(UserSkill.objects.filter(user__in=qs).values_list('name', flat=True).order_by('name').distinct())
+            context['cities'] = list(User.objects.filter(id__in=qs).exclude(is_staff=True).values_list('profile__location', flat=True).order_by('profile__location').distinct())
+        return context
